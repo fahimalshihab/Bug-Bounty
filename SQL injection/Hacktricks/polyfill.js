102d90fa3f12ef21b5ca17f57a20da533ad9c91f
fahim al shihab <97816146+fahimalshihab@users.noreply.github.com>
2023-11-10 00:09:51 +0600

    Add files via upload

---
+++ SQL injection/Hacktricks/polyfill.js
@@ -0,0 +1,9 @@
+/* Polyfill service v3.25.1
+ * For detailed credits and licence information see https://github.com/JakeChampion/polyfill-service.
+ * 
+ * Features requested: Intl.~locale.en
+ *  */
+
+
+/* No polyfills needed for current settings and browser */
+
